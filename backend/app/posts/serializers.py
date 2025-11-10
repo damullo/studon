9a9f223fa7ff020ce22097519def34bf9c1ab1cc
Ivan <damullo1985@gmail.com>
2025-11-10 02:15:39 +0200

    Add read-only _id field to PostSerializer

--- backend/app/posts/serializers.py
+++ backend/app/posts/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
+    _id = serializers.CharField(read_only=True)
+
     class Meta:
         model = Post
         fields = "__all__"
