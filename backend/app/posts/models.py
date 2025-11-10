7cb62963eb057f4c2b76993a61579f6b6b2a22e4
Ivan <damullo1985@gmail.com>
2025-11-10 02:14:46 +0200

    Add ObjectIdField for MongoDB integration

--- backend/app/posts/models.py
+++ backend/app/posts/models.py
@@ -1,7 +1,23 @@
 from djongo import models
 from django.utils import timezone
+from bson import ObjectId
+
+class ObjectIdField(models.Field):
+    def __init__(self, *args, **kwargs):
+        kwargs["editable"] = False
+        super().__init__(*args, **kwargs)
+
+    def get_prep_value(self, value):
+        return str(value) if isinstance(value, ObjectId) else value
+
+    def from_db_value(self, value, expression, connection):
+        return str(value)
+
+    def to_python(self, value):
+        return str(value)
 
 class Post(models.Model):
+    _id = ObjectIdField(primary_key=True)
     author = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(default=timezone.now)
